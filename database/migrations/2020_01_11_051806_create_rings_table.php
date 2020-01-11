328d7ed9a2e19e11abdf1aa562231acb0e02456b
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-11 14:19:42 +0900

    add name column to Ring

--- database/migrations/2020_01_11_051806_create_rings_table.php
+++ database/migrations/2020_01_11_051806_create_rings_table.php
@@ -15,6 +15,7 @@ class CreateRingsTable extends Migration
     {
         Schema::create('rings', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
