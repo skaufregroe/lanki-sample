2f04560f8a1dd91cee8e256619c89d4c88832271
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-11 14:15:51 +0900

    add jumbotron

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -2,5 +2,10 @@
 
 
 @section('content')
-    <h1>Lanki Samples</h1>
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, Lanki Sample!</h1>
+        <p class="lead">samle anki app!</p>
+        <hr class="my-4">
+        <a class="btn btn-primary btn-lg" href="{{route('login')}}" role="button">Login</a>
+    </div>
 @endsection
