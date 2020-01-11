83c0c4eba1f923f2467dfdb1ed6bbbbd113c0dca
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-11 14:12:46 +0900

    using container class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,9 +25,11 @@
         @component('layouts.header')
         @endcomponent
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="container">
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
     </div>
 </body>
 </html>
